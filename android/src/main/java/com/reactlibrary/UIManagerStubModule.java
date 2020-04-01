5372ca666016126a97b3ea4b8c38a9ef52e1bc4c
Maxim Gladkov <contact@maximgladkov.com>
2020-04-01 19:58:37 +0300

    add dummy react method

--- android/src/main/java/com/reactlibrary/UIManagerStubModule.java
+++ android/src/main/java/com/reactlibrary/UIManagerStubModule.java
@@ -3,6 +3,7 @@ package com.reactlibrary;
 import com.facebook.react.bridge.OnBatchCompleteListener;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
 
 public class UIManagerStubModule extends ReactContextBaseJavaModule implements OnBatchCompleteListener {
 
@@ -18,5 +19,9 @@ public class UIManagerStubModule extends ReactContextBaseJavaModule implements O
     @Override
     public void onBatchComplete() {
 
+    }
+    
+    @ReactMethod
+    public void dummy() {
     }
 }
