6a8ddc6479ae730410feceb8d776cd5e7c080d9d
Jacob Parker <jacobparker1992@gmail.com>
2020-11-11 17:50:52 +0000

    Documentation

--- js/react-native-stripped.js
+++ js/react-native-stripped.js
@@ -1,13 +1,16 @@
 /*
- * The minimum amount of imports/exports required to get a thread to run
+ * The minimum amount of setup and imports required to get a thread to run
  * These references are copied from react-native/index.js
  */
 import BatchedBridge from 'react-native/Libraries/BatchedBridge/BatchedBridge';
 import NativeModules from 'react-native/Libraries/BatchedBridge/NativeModules';
 import DeviceEventEmitter from 'react-native/Libraries/EventEmitter/RCTDeviceEventEmitter';
 
-// This is safe to call multiple times with the same arguments
-// Should the user `import 'react-native'`
+/*
+ * See `Libraries/Core/setUpBatchedBridge.js`
+ * There is no impact for calling this function twice with the same arguments
+ * So it is still safe for the user to `import 'react-native'`
+ */
 BatchedBridge.registerCallableModule('RCTDeviceEventEmitter', DeviceEventEmitter);
 
 export {
