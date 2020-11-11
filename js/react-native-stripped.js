4c9a073c551b7990be2e2980ca43101261b0c0d4
Jacob Parker <jacobparker1992@gmail.com>
2020-11-11 18:04:31 +0000

    Add more missing setup

--- js/react-native-stripped.js
+++ js/react-native-stripped.js
@@ -2,6 +2,7 @@
  * The minimum amount of setup and imports required to get a thread to run
  * These references are copied from react-native/index.js
  */
+import 'react-native/Libraries/Core/setUpGlobals'
 import BatchedBridge from 'react-native/Libraries/BatchedBridge/BatchedBridge';
 import NativeModules from 'react-native/Libraries/BatchedBridge/NativeModules';
 import DeviceEventEmitter from 'react-native/Libraries/EventEmitter/RCTDeviceEventEmitter';
@@ -11,7 +12,24 @@ import DeviceEventEmitter from 'react-native/Libraries/EventEmitter/RCTDeviceEve
  * There is no impact for calling this function twice with the same arguments
  * So it is still safe for the user to `import 'react-native'`
  */
+BatchedBridge.registerLazyCallableModule(
+  'RCTLog',
+  () => require('react-native/Libraries/Utilities/RCTLog')
+);
 BatchedBridge.registerCallableModule('RCTDeviceEventEmitter', DeviceEventEmitter);
+BatchedBridge.registerLazyCallableModule(
+  'RCTNativeAppEventEmitter',
+  () => require('react-native/Libraries/EventEmitter/RCTNativeAppEventEmitter')
+);
+/*
+ * This module differs from what will happen in the default setup
+ * (At least in development)
+ * But as hot module reloading does not work in threads, this shouldn't matter
+ */
+BatchedBridge.registerLazyCallableModule(
+  'HMRClient',
+  () => require('react-native/Libraries/Utilities/HMRClientProdShim')
+);
 
 export {
   NativeModules,
