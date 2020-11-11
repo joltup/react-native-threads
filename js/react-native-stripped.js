ffb0687724a21dd4a552f72d5a05f55e91c1323b
Jacob Parker <jacobparker1992@gmail.com>
2020-11-11 17:18:09 +0000

    Add import to react-native setup

--- js/react-native-stripped.js
+++ js/react-native-stripped.js
@@ -2,6 +2,7 @@
  * The minimum amount of imports/exports required to get a thread to run
  * These references are copied from react-native/index.js
  */
+import 'react-native/Libraries/Core/setUpBatchedBridge';
 import NativeModules from 'react-native/Libraries/BatchedBridge/NativeModules';
 import DeviceEventEmitter from 'react-native/Libraries/EventEmitter/RCTDeviceEventEmitter';
 
